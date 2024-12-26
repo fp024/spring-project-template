f4b0dbf153bb02df4b46f73a35c51fb305bbedcd
fp024 <aopenasus@naver.com>
2024-12-26 18:55:03 +0900

    linux용 템플릿 프로젝트 스크립트 추가, 동작확인

--- test/make-spring5-project.sh
+++ test/make-spring5-project.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 GROUP_ID=org.fp024.sample
-ARTIFACT_ID=spring6-mvc-sample
+ARTIFACT_ID=spring5-mvc-sample
 PACKAGE=org.fp024.sample
 
 echo ### Create ${ARTIFACT_ID} Project ###
@@ -26,7 +26,7 @@ fi
 
 ${MAVEN_HOME}/bin/mvn archetype:generate \
   -DarchetypeGroupId=org.fp024.archetype \
-  -DarchetypeArtifactId=spring6-mvc-project-archetype \
+  -DarchetypeArtifactId=spring5-mvc-project-archetype \
   -DarchetypeVersion=1.0.0-SNAPSHOT \
   -DgroupId=${GROUP_ID} \
   -DartifactId=${ARTIFACT_ID} \
