aed2f769dd1f8c71d5aedb3d76a07deae3dbdd0a
fp024 <aopenasus@naver.com>
2024-12-29 05:53:48 +0900

    context root를 /로 설정 - spring5-mvc-project

--- archetypes/spring5-mvc-project-archetype/src/main/resources/archetype-resources/tomcat-run.bat
+++ archetypes/spring5-mvc-project-archetype/src/main/resources/archetype-resources/tomcat-run.bat
@@ -11,6 +11,7 @@ SET MAVEN_OPTS="-Dfile.encoding=UTF-8"
 :: Maven 명령어 실행
 mvn clean verify org.codehaus.cargo:cargo-maven3-plugin:run ^
   -DskipTests ^
+  -DcargoContextPath=%cargo_context_path% ^
   -Dcargo.jvmargs="%cargo_jvmargs%" ^
   -Dcargo.maven.containerId=%cargo_maven_containerId% ^
   -Dcargo.maven.containerUrl=https://repo.maven.apache.org/maven2/org/apache/tomcat/tomcat/%cargo_tomcat_version%/tomcat-%cargo_tomcat_version%.zip ^
